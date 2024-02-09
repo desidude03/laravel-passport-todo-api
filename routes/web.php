a706c26df6c5705b4b337ac728994581b14dfb59
Sayed Muzammil <smuzammil@teckborn.com>
2024-02-09 10:30:31 +0530

    remove sub repo

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/unauthenticated', function () {
-    return response()->json(['message' => 'Unauthenticated.'], 403);
-})->name('unauthenticated');
